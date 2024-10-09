50d8340e5f6a4094ac0688d077257277a915a7dd
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-10-09 13:10:19 +0300

    making my register project

--- my-gaming-app-16/src/app/app.module.ts
+++ my-gaming-app-16/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
