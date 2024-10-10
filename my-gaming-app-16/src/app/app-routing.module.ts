1ddf758dc1ecc5293aa95230161cc42fe5526e55
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-10-10 12:35:29 +0300

    adding my home page html+ css

--- my-gaming-app-16/src/app/app-routing.module.ts
+++ my-gaming-app-16/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
