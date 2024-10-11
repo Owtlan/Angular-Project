8df2a4b76cd435f2eefd6a593874f35b2f5ae015
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-10-11 16:00:23 +0300

    register user settings

--- my-gaming-app-16/src/app/app.module.ts
+++ my-gaming-app-16/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // За ngModel
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { RegisterComponent } from '../app/user/register/register.component';
-import { LoginComponent } from '../app/user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
 import { HomeComponent } from './home/home.component';
-
+import { RouterModule } from '@angular/router';
+// Firebase imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,15 @@ import { HomeComponent } from './home/home.component';
     RegisterComponent,
     LoginComponent,
     HomeComponent,
-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // За ngModel
+    RouterModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), // Инициализиране на Firebase
+    provideAuth(() => getAuth()), // Firebase Authentication модул
+    provideFirestore(() => getFirestore()), // Firestore модул
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-gaming-app-16/src/app/user/register/register.component.ts
+++ my-gaming-app-16/src/app/user/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { Firestore, doc, setDoc } from '@angular/fire/firestore'; // За Firestore
+import { Router } from '@angular/router'; // За пренасочване след успешна регистрация
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  email: string = '';
+  password: string = '';
+  username: string = '';
+
+  constructor(private auth: Auth, private firestore: Firestore, private router: Router) { }
+
+  register() {
+    createUserWithEmailAndPassword(this.auth, this.email, this.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+
+        // Записване на потребителя в Firestore
+        const userDocRef = doc(this.firestore, 'users', user.uid);
+        return setDoc(userDocRef, {
+          email: this.email,
+          username: this.username
+        });
+      })
+      .then(() => {
+        console.log('User registered and data saved to Firestore');
+        this.router.navigate(['/']); // Пренасочване след успешна регистрация
+      })
+      .catch((error) => {
+        console.error('Error registering user: ', error);
+      });
+  }
 }
