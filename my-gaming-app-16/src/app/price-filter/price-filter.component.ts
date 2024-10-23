333a569cd210640957cd05a8d7835ac6ba5358c8
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-10-23 13:52:49 +0300

    adding logic about cart and details in my price-filter component

--- my-gaming-app-16/src/app/price-filter/price-filter.component.ts
+++ my-gaming-app-16/src/app/price-filter/price-filter.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
 import { Game } from '../model/game.model';
-
+import { Auth, user } from '@angular/fire/auth';
+import { Firestore, doc, getDocs, collection, query, where, limit } from '@angular/fire/firestore';
+import { CartService } from '../service/cart.service'; 
 
 @Component({
   selector: 'app-price-filter',
@@ -17,21 +19,67 @@ export class PriceFilterComponent implements OnInit {
   filteredGames: Game[] = [];
   minPrice: number = 0;
   maxPrice: number = 100;
-  maxPriceLimit: number = 100;
+  maxPriceLimit: number = 500;
+  isLoggedIn: boolean = false;
+  currentUserId: string | null = null;
+  purchasedGames: string[] = [];
 
 
-
-  constructor(private firebaseService: FirebaseService) { }
+  constructor(private firebaseService: FirebaseService, private auth: Auth, private cartService: CartService,) { }
 
   ngOnInit(): void {
     this.firebaseService.getGames().subscribe(games => {
       this.games = games;
       this.filteredGames = games;
     })
+
+    user(this.auth).subscribe(user => {
+      if (user) {
+        this.isLoggedIn = true;
+        this.currentUserId = user.uid;
+        this.loadPurchasedGames(); 
+      } else {
+        this.isLoggedIn = false;
+        this.currentUserId = null;
+      }
+    });
   }
+
 
   filteredGamesByPrice(): void {
     this.filteredGames = this.games.filter(game => game.price >= this.minPrice && game.price <= this.maxPrice);
   }
 
+
+  async loadPurchasedGames() {
+    if (!this.currentUserId) return;
+
+    const ordersCollection = collection(this.firebaseService.firestore, 'orders');
+    const q = query(ordersCollection, where('userId', '==', this.currentUserId));
+    const ordersSnapshot = await getDocs(q);
+
+    this.purchasedGames = ordersSnapshot.docs.map(doc => doc.data()['gameId']);
+  }
+
+
+  isGamePurchased(gameId: string): boolean {
+    return this.purchasedGames.includes(gameId);
+  }
+
+
+  isGameCreatedByUser(game: Game): boolean {
+    return this.currentUserId === game.creatorId; // Проверка дали играта е създадена от текущия потребител
+  }
+
+  addToCart(game: Game): void {
+    if (this.currentUserId) {
+      // Добави логиката за добавяне на играта в количката тук
+      this.cartService.addToCart(game, this.currentUserId);
+      this.cartService.updateCartItemCount(this.currentUserId); 
+      console.log(`Added to cart: ${game.title}`);
+      alert(`${game.title} беше добавена в количката.`);
+    } else {
+      alert('Трябва да сте логнат, за да добавите игра в количката.');
+    }
+  }
 }
