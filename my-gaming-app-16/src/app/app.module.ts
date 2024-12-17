96eb08a950914379e4b15a7b9f869ac17f8fc8d8
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-12-17 12:47:00 +0200

    change path

--- my-gaming-app-16/src/app/app.module.ts
+++ my-gaming-app-16/src/app/app.module.ts
@@ -3,29 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // За ngModel
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
 import { RegisterComponent } from './user/register/register.component';
 import { LoginComponent } from './user/login/login.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './core/home/home.component';
 import { RouterModule } from '@angular/router';
-// Firebase imports
+
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideStorage, getStorage } from '@angular/fire/storage'; // Импорт на Storage
+import { provideStorage, getStorage } from '@angular/fire/storage'; 
 
 import { environment } from '../environments/environment';
 import { LogoutComponent } from './user/logout/logout.component';
 import { CreateComponent } from './create/create.component';
-import { GameDetailsComponent } from './game-details/game-details.component';
-import { GamesComponent } from './games/games.component';
+import { GameDetailsComponent } from './game core/game-details/game-details.component';
+import { GamesComponent } from './game core/games/games.component';
 
 import { EditGameComponent } from './edit-game/edit-game.component';
 import { OrderComponent } from './order/order.component';
 import { CartComponent } from './cart/cart.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { PriceFilterComponent } from './price-filter/price-filter.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 
 
